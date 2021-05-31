0bc795863aaff499b21b96ce9795aafa5897b566
Arif <arifadi952@gmail.com>
2021-05-31 22:51:57 +0600

    This is test code

---
+++ NewClass.java
@@ -0,0 +1,16 @@
+
+
+package javaapplication;
+
+
+
+public class NewClass {
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(i+" HelloJana");
+        }
+    }
+}
